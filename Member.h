f2b9434037bac391d30feda33ac6c14b835becdc
amitp <amitp01@walla.com>
2018-04-08 15:17:03 +0300

    sdfsdf

--- Member.h
+++ Member.h
@@ -1,35 +1,13 @@
+#include <vector>
 #ifndef AA_hpp
 #define AA_hpp
 class Member
 {
     Member(){};
-    void follow (const Member &other);
-    int count();
-    void unfollow(const Member &other);
-    int numFollowers();
-    int numFollowing();
+    void follow (const Member &other){}
+    int count(){return 0}
+    void unfollow(const Member &other){}
+    int numFollowers(){return 0}
+    int numFollowing(){return 0}
 };
-#include <vector>
-void Member::follow(const Member &other)
-{
-
-}
-
-int Member::count() {
-    return 0;
-}
-
-
-
-void Member::unfollow(const Member &other) {
-
-}
-
-int Member::numFollowers() {
-    return 0;
-}
-
-int Member::numFollowing() {
-    return 0;
-}
 #endif
