65caaec54cdef3d55ac31cd943006006ebbe4bf3
Amit Palti <nomq1@users.noreply.github.com>
2018-04-08 22:07:13 +0300

    update

--- Member.cpp
+++ Member.cpp
@@ -1,5 +1,30 @@
-//
-// Created by amit on 3/20/2018.
-//
-
 #include "Member.h"
+
+int Member::_count = 0;
+
+Member::Member() {
+	++_count;
+}
+
+Member::~Member() {
+	for (Member* m: following) {
+		this->unfollow(*m);
+	}
+	for (Member* m: followers) {
+		m->unfollow(*this);
+	}
+	--_count;
+}
+
+void Member::follow(Member& other) {
+	if( &other != this)
+{
+	following.insert(&other);
+	other.followers.insert(this);
+}
+}
+
+void Member::unfollow(Member& other) {
+	following.erase(&other);
+	other.followers.erase(this);
+}
