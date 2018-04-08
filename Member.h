8b5a6ff16eaf8abc88931cc09841d08e162e3b76
Amit Palti <nomq1@users.noreply.github.com>
2018-04-08 22:07:45 +0300

    update

--- Member.h
+++ Member.h
@@ -1,15 +1,23 @@
-#include <vector>
-#ifndef AA_hpp
-#define AA_hpp
-class Member
-{
-    public:
-    Member(){};
-    void follow (const Member &other){}
-    static int count(){return 0;}
-    void unfollow(const Member &other){}
-    int numFollowers(){return 0;}
-    int numFollowing(){return 0;}
+#pragma once
 
+#include <set>
+#include <string>
+using namespace std;
+
+class Member {
+	set<Member*> following;  // members that I follow
+	set<Member*> followers;  // members that follow me
+	static int _count;  // total number of members in network
+
+public:
+	Member();
+	~Member();
+
+	void follow   (Member& other);
+	void unfollow (Member& other);
+
+	int numFollowing() const { return following.size();	}
+	int numFollowers() const { return followers.size();	}
+
+	static int count() { return _count; }
 };
-#endif
